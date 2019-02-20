e3d93f3366dd32fad649c7db3a809efbbe9aea52
tung_tvt <trinhvutung1987@gmail.com>
2019-02-20 23:46:18 +0700

    get linkMp4 from data client

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ link git: "https://github.com/jakett/dv3s-singal-server.git"
 
 # Usages:
 signal server for webrtc app
+
+# Server:
+https://server-dv3s.herokuapp.com:443
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const fs = require('fs');
+const ytdl = require('ytdl-core');
 
 var port = process.env.PORT || 4000;
 
@@ -9,29 +11,29 @@ var UserManager = {
     socketList: []
 }
 
-io.sockets.on('connection', function(socket) {
+io.sockets.on('connection', function (socket) {
     console.log("A User is connected, socketId = " + socket.id);
-    console.log(socket.handshake.headers);
+    // console.log(socket.handshake.headers);
     var object = {};
     object.type = 'welcome';
     object.id = socket.id;
     object.socket = socket.handshake.headers;
     sendMessageToClient(socket, object);
-    
-    socket.on('message', function(data) { handleDataReceive(data, socket); });
-    socket.on('disconnect', function() { handleDisconnectUser(socket); });
-    
+
+    socket.on('message', function (data) { handleDataReceive(data, socket); });
+    socket.on('disconnect', function () { handleDisconnectUser(socket); });
+
     /* -------------------- FUNCTION DEATAILS -------------------- */
-    
+
     function handleDisconnectUser(socket) {
-        var index = (UserManager.socketList).map(function(item) { return item.id; }).indexOf(socket.id);
-        if(index !== -1) {
+        var index = (UserManager.socketList).map(function (item) { return item.id; }).indexOf(socket.id);
+        if (index !== -1) {
             var typeDevice = UserManager.userList[index].type_device;
-            
+
             (UserManager.socketList).splice(index, 1);
             (UserManager.userList).splice(index, 1);
 
-            if(typeDevice === 'player') {
+            if (typeDevice === 'player') {
                 var object = {};
                 object.type = 'remove_player';
                 object.id = socket.id;
@@ -39,23 +41,23 @@ io.sockets.on('connection', function(socket) {
             }
         }
     }
-    
+
     function handleDataReceive(data, socket) {
-        switch(data.type) {
+        switch (data.type) {
             case 'user_info':
-                var index = (UserManager.socketList).map(function(item) { return item.id; }).indexOf(socket.id);
-                if(index !== -1) { return; }
-                
+                var index = (UserManager.socketList).map(function (item) { return item.id; }).indexOf(socket.id);
+                if (index !== -1) { return; }
+
                 var object = {};
                 var user = {};
                 user.type_device = data.data;
                 user.id = socket.id;
                 user.status = 'disconnected';
                 UserManager.userList.push(user);
-                
+
                 UserManager.socketList.push(socket);
-                
-                if(user.type_device === 'player') {
+
+                if (user.type_device === 'player') {
                     object.type = 'add_player';
                     object.id = socket.id;
                     sendMessageToGlobal(socket, object);
@@ -72,14 +74,18 @@ io.sockets.on('connection', function(socket) {
                 break;
             case 'leave':
                 break;
+            case 'link':
+                console.log("link youtube: " + data.path);
+                sendLinkMp4ToClient(data.path);
+                break;
             default:
         }
     }
-    
+
     function handleEvent(socketId, data) {
         var destinationId = data.id;
         var socketClient = getSocketClient(destinationId);
-        if(socketClient !== null) {
+        if (socketClient !== null) {
             var object = {};
             object.id = socketId;
             object.type = data.type;
@@ -87,29 +93,51 @@ io.sockets.on('connection', function(socket) {
             sendMessageToClient(socketClient, object);
         }
     }
-    
+
     function getSocketClient(id) {
         var socketClient = null;
-        var idx = UserManager.socketList.map(function(item) { return item.id; }).indexOf(id);
-        if(idx !== -1) {
+        var idx = UserManager.socketList.map(function (item) { return item.id; }).indexOf(id);
+        if (idx !== -1) {
             socketClient = UserManager.socketList[idx];
         }
         return socketClient;
     }
-    
+
     function sendMessageToClient(socket, dataSend) {
         socket.emit('message', dataSend);
     }
-    
+
     function sendMessageToGlobal(socket, dataSend) {
         socket.broadcast.emit('message', dataSend);
     }
-})  
 
-app.get('/', function(req, res){
+    function sendLinkMp4ToClient(link) {
+        ytdl.getInfo(link, (err, info) => {
+            if(err) { throw err };
+            const arr = info.formats.filter(item => item.container === 'mp4');
+            const idx = getIndexOfMaxResolution(arr);
+            const linkMp4 = arr[idx].url;
+            console.log("TVT - linkMp4 = " + linkMp4);
+        })
+    }
+
+    function getIndexOfMaxResolution(formatArr) {
+        const resolutionArr = formatArr.map(item => {
+            let resolutionStr = item.resolution;
+            resolutionStr = resolutionStr.substring(0, resolutionStr.length - 1);
+            const resolutionInt = parseInt(resolutionStr);
+            return resolutionInt;
+        });
+
+        console.log("TVT - max resolution = " + Math.max(... resolutionArr));
+        return resolutionArr.findIndex(item => item === Math.max(...resolutionArr));
+    }
+})
+
+app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function () {
+    console.log('listening on *:' + port);
 });
